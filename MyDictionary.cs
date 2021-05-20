6bf80365921e078671a675d2da456aed2c5f56ba
Tayfun Temurtaş <tayfun_temurtas@hotmail.com>
2021-05-20 18:04:31 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericDictioanary
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private TKey[] _key;
+        private TValue[] _value;
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+
+        public void Add(TKey keys, TValue values)
+        {
+            TKey[] tempKey = _key;
+            TValue[] tempValue = _value;
+
+            _key = new TKey[_key.Length + 1];
+            _value = new TValue[_value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                _key[i] = tempKey[i];
+            }
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                _value[i] = tempValue[i];
+            }
+
+            _key[_key.Length - 1] = keys;
+            _value[_value.Length - 1] = values;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericDictioanary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "tayfun");
+            myDictionary.Add(2, "tayfun");
+
+
+        }
+    }
+}
